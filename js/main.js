0c02ea8dd56912197f4f1b82f440b45b237970ef
Big Fame Deployer <deployer@bigfame.com.tw>
2026-08-05 02:03:19 +0800

    Version inquiry context update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /* Big Fame IND. CORP. - Global JavaScript Logic */
-var SITE_VERSION = '1.3.19';
+var SITE_VERSION = '1.3.20';
 
 document.addEventListener('DOMContentLoaded', () => {
   initAnalytics();
